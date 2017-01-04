e24bda536f2051f6928a222fda9b709bef18c5e9
dianalo <dianalo@student.ethz.ch>
2017-01-04 18:42:43 +0100

    added youtube video

--- src/php/search.php
+++ src/php/search.php
@@ -1,6 +1,12 @@
 <?php
 error_reporting(E_ALL & ~E_WARNING);
-$url = search("kids");
+//error_reporting(E_ALL);
+
+header('Content-type: text/html; charset=utf-8');
+$song = search("abba");
+
+$songname = $song[0];
+$url = $song[1];
 //echo $url;
 
 //fetch ultimate guitar chord webpage of queried song
@@ -8,11 +14,15 @@ $handle = curl_init($url);
 curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
-$res = curl_exec($handle);
+$res = utf8_decode(curl_exec($handle));
 //var_dump($res);
 
 //trim it
-echo getSongContent($res);
+//echo getSongContent($res);
+$dom = injectIntoHTML("chordtext", getSongContent($res), null);
+$dom = injectIntoHTML("video", getVideoDOM(trim($songname)), $dom);
+
+echo $dom->saveHTML();
 
 
 //returns ultimate guitar homepage with best rating among all results from query q
@@ -25,7 +35,7 @@ function search($q)
   curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
-  $res = curl_exec($handle);
+  $res = utf8_decode(curl_exec($handle));
 
   //$errmsg  = curl_error($handle);
 
@@ -41,16 +51,17 @@ function search($q)
 
   //find maximum songrating
   $maxRating = 0;
-  $maxElem = null;
+  $maxURL = null;
   for($i=0; $i<$ratings->length; $i++){
     if($ratings[$i]->nodeValue > $maxRating){
       $maxRating = $ratings[$i];
-      $maxElem = $songs[$i]->getAttribute('href');
+      $maxURL = $songs[$i]->getAttribute('href');
+      $maxName = $songs[$i]->nodeValue;
     }
   }
 
-  return $maxElem;
   //var_dump($maxElem);
+  return array($maxName, $maxURL);
 }
 
 function getSongContent($htmlString){
@@ -59,6 +70,10 @@ function getSongContent($htmlString){
 
   $xpath = new DOMXPath($html);
   $trimmed = $xpath->query("//div[@id='cont']//pre[@class='js-tab-content']")->item(0);
+  // var_dump($trimmed);
+  // if($trimmed == ""){
+  //   $trimmed = $xpath->query("//div[@id='cont']//pre[@class='js-tab-content']")->nodeValue;
+  // }
 
   //convert to HTML string
   $trimmedString = "";
@@ -66,8 +81,63 @@ function getSongContent($htmlString){
   $cloned = $trimmed->cloneNode(TRUE);
   $newdoc->appendChild($newdoc->importNode($cloned,TRUE));
   $trimmedString .= $newdoc->saveHTML();
+  //var_dump($trimmedString);
 
   return $trimmedString;
 
+}
+
+function getVideoDOM($q){
+  $value = str_replace(' ', '+', $q);
+  //get search results from ultimate guitar
+
+  $handle = curl_init("https://www.youtube.com/results?search_query=".$value);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+  $res = utf8_decode(curl_exec($handle));
+
+  //$errmsg  = curl_error($handle);
+
+  $html = new DOMDocument();
+  $html->loadHTML($res);
+  //var_dump($html);
+  $finder = new DOMXPath($html);
+  //$nodes = $finder->query("b[@class='ratdig']");
+  //ol[@class='item-section']
+  $firstRes = $finder->query("//div[@id='results']//div[@data-context-item-id]")->item(0);
+  //var_dump($firstRes);
+  //$videoID = $firstRes->hasAttribute("data-context-item-id");
+  $videoID = $firstRes->getAttribute("data-context-item-id");
+
+  //var_dump($videoID);
+
+  $videoDOM = new DOMDocument();
+  $videoDOM->loadHTML('<iframe width="420" height="315"
+src="https://www.youtube.com/embed/'.$videoID.'">
+</iframe>');
+  return $videoDOM->saveHTML();
+}
+
+function injectIntoHTML($parentId, $injectedHTML, $dom){
+
+  if($dom == null){
+    $dom = new DOMDocument();
+    $dom->loadHTMLFile("../HTML/layout.html");
+  }
+
+  $parent = $dom->getElementById($parentId);
+  //create fragment, append HTML to it & append it to node -> only works for correctly formed html
+  // $frag = $dom->createDocumentFragment();
+  // $frag->appendXML($injectedHTML);
+  // var_dump($frag);
+  // $parent->appendChild($frag);
+
+  //workaround for malformed HTML: load new HTML into DOMDocument and append it
+  $inDom = new DOMDocument();
+  $inDom->loadHTML($injectedHTML);
+  $parent->appendChild($dom->importNode($inDom->documentElement, true));
+
+  return $dom;
 }
 ?>
