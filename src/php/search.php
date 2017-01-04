7e4690c83a5c3d1eef5958cb9a40470edc6378bb
dianalo <dianalo@student.ethz.ch>
2017-01-04 20:36:06 +0100

    added search field

--- src/php/search.php
+++ src/php/search.php
@@ -1,29 +1,37 @@
 <?php
 error_reporting(E_ALL & ~E_WARNING);
 //error_reporting(E_ALL);
-
 header('Content-type: text/html; charset=utf-8');
-$song = search("abba");
 
-$songname = $song[0];
-$url = $song[1];
-//echo $url;
+if(!array_key_exists("name", $_GET)){
+  $basislayout = new DOMDocument();
+  $basislayout->loadHTMLFile("../HTML/layout.html");
+  echo $basislayout->saveHTML();
+}
+else{
+  $name = htmlspecialchars($_GET["name"]);
 
-//fetch ultimate guitar chord webpage of queried song
-$handle = curl_init($url);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
-$res = utf8_decode(curl_exec($handle));
-//var_dump($res);
+  $song = search($name);
 
-//trim it
-//echo getSongContent($res);
-$dom = injectIntoHTML("chordtext", getSongContent($res), null);
-$dom = injectIntoHTML("video", getVideoDOM(trim($songname)), $dom);
+  $songname = $song[0];
+  $url = $song[1];
+  //echo $url;
 
-echo $dom->saveHTML();
+  //fetch ultimate guitar chord webpage of queried song
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+  $res = utf8_decode(curl_exec($handle));
+  //var_dump($res);
 
+  //trim it
+  //echo getSongContent($res);
+  $dom = injectIntoHTML("chordtext", getSongContent($res), null);
+  $dom = injectIntoHTML("video", getVideoDOM(trim($songname)), $dom);
+
+  echo $dom->saveHTML();
+}
 
 //returns ultimate guitar homepage with best rating among all results from query q
 function search($q)
